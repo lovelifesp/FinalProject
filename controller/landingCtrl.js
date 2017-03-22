08ca1e4f77f7b4998dd7a94ff57fad6b9e4d3df0
Shana Parker <lovelifesp@gmail.com>
2017-03-22 15:09:45 -0400

    added code generate random store

--- controller/landingCtrl.js
+++ controller/landingCtrl.js
@@ -6,31 +6,51 @@ app.controller("landingCtrl", function($scope, $http){
   // var infowindow;
 
 
-  function initMap() {
-    var detroit = {lat:  42.331429, lng: -83.045753};
 
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: detroit,
-      zoom: 15
-    });
 
-    infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-      location: detroit,
-      radius: 500,
-      type: ['clothing_store']
-    }, function callback(results, status) {
-      $scope.$apply(function(){
-        $scope.storeInfo = results;
+
+
+    function initMap() {
+      var detroit = {lat:  42.331429, lng: -83.045753};
+
+
+      var service = new google.maps.places.PlacesService(map);
+
+      service.nearbySearch({
+        location: detroit,
+        radius: 500,
+        type: ['clothing_store']
+      },function callback(results, status) {
+        $scope.$apply(function(){
+
+          // Code to handle result from API goes here.
+          // results is the array of stores
+
+          // $scope.randomStore= $scope.storeInfo(Math.random() * $scope.results);
+          $scope.storeInfo = results;
+
+          $scope.randomStore= $scope.storeInfo[Math.floor(Math.random() * $scope.storeInfo.length)];
+
+
+
+
+          // console.log("results", results);
+          console.log("randomStore", $scope.randomStore);
+
+
+          // Code to handle results from API ends here.
+        });
+
+
+
+
+
       });
-      console.log(results);
+
+
+    }
+
+
+      initMap();
+
     });
-  }
-
-
-
-
-  initMap();
-
-});
